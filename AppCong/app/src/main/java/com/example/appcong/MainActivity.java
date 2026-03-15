eb180424859abcc7562b2d918f518edb24a07dac
vudat169 <datvu2990@gmail.com>
2026-03-15 21:07:12 +0700

    hoan thien app
    app cong

--- AppCong/app/src/main/java/com/example/appcong/MainActivity.java
+++ AppCong/app/src/main/java/com/example/appcong/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.appcong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void XuLyCong(View view){
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+        //lay du lieu ve so A
+        String strA = editTextSoA.getText().toString();
+        //lay du lieu ve so B
+        String strB = editTextSoB.getText().toString();
+        //chuyen du lieu sang so
+        int so_A= Integer.parseInt(strA);
+        int so_B= Integer.parseInt(strB);
+        //tinh toan theo yeu cau
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);
+        //hien thi ket qua
+        editTextKetQua.setText(strTong);
     }
 }
