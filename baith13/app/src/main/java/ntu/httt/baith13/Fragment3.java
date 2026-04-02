cf7ebd82c85641863058350d9eb0b12f6e11c2b2
vudat169 <datvu2990@gmail.com>
2026-04-02 21:32:46 +0700

    bài th10, bài th13
    thêm bài th

---
+++ baith13/app/src/main/java/ntu/httt/baith13/Fragment3.java
@@ -0,0 +1,28 @@
+package ntu.httt.baith13;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment3 extends Fragment {
+
+    public Fragment3() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_3, container, false);
+    }
+}
