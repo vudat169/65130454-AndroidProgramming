0ce1928a98b6236cf9f05c01377202f99e830460
vudat169 <datvu2990@gmail.com>
2026-04-20 08:06:35 +0700

    commit 4
    commit lần 4

--- thigk2/app/src/main/java/thigk2/VuDoanThanhDat/ChucNang1.java
+++ thigk2/app/src/main/java/thigk2/VuDoanThanhDat/ChucNang1.java
@@ -1,4 +1,43 @@
 package thigk2.VuDoanThanhDat;
 
-public class ChucNang1 {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChucNang1 extends AppCompatActivity {
+    private EditText edtVND;
+    private Button btnConvert;
+    private TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chuc_nang_1);
+
+        edtVND = findViewById(R.id.edtVND);
+        btnConvert = findViewById(R.id.btnConvert);
+        txtResult = findViewById(R.id.txtResult);
+
+        btnConvert.setOnClickListener(v -> {
+            String input = edtVND.getText().toString().trim();
+
+            if (input.isEmpty()) {
+                Toast.makeText(this, "Vui lòng nhập số tiền!", Toast.LENGTH_SHORT).show();
+            } else {
+                try {
+                    double vnd = Double.parseDouble(input);
+                    double usd = vnd / 25000;
+
+                    // Hiển thị kết quả lấy 2 số thập phân
+                    txtResult.setText(String.format("Kết quả: %.2f USD", usd));
+                } catch (NumberFormatException e) {
+                    Toast.makeText(this, "Vui lòng nhập số hợp lệ!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 }
