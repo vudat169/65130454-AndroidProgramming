c53a7ef9139ddb6e8e7808d678e4b6aa97c8621c
vudat169 <datvu2990@gmail.com>
2026-04-20 07:51:22 +0700

    commit 2
    commit lần 2

--- thigk2/app/src/main/java/thigk2/VuDoanThanhDat/MainActivity.java
+++ thigk2/app/src/main/java/thigk2/VuDoanThanhDat/MainActivity.java
@@ -1,6 +1,8 @@
 package thigk2.VuDoanThanhDat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn1,btn2,btn3,btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btn1.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ChucNang1.class);
+            startActivity(intent);
+        });
+
     }
 }
