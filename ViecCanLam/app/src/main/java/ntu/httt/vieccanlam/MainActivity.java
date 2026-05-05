47488ce264661f24ec0e2d422392567146c7b0e2
vudat169 <datvu2990@gmail.com>
2026-05-05 22:11:03 +0700

    bài todo list
    bài thực hành todo list

---
+++ ViecCanLam/app/src/main/java/ntu/httt/vieccanlam/MainActivity.java
@@ -0,0 +1,56 @@
+package ntu.httt.vieccanlam;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<TASKS> lstVCL;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //tạo csdl firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference("TASKS");
+        //lắng nghe và xử lý
+        lstVCL = new ArrayList<TASKS>();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //lấy dữ liệu
+                for(DataSnapshot obj: snapshot.getChildren() ){
+                    TASKS task = obj.getValue(TASKS.class);
+                    lstVCL.add(task);
+                    Log.w("VCL app","tên việc: "+ task.getName() );
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
